572969edf136b21a49b18911ab039c4f1b63efd1
Terranslayer <zyf352943910@gmail.com>
2022-11-26 02:23:42 -0500

    placeholder

--- train.py
+++ train.py
@@ -194,8 +194,7 @@ if __name__ == "__main__":
         print("world size: ", world_size)
         print("ngpus per node: ", ngpus_per_node)
         print("job id: ", job_id)
-    context = mp.spawn(model_init, args=(ngpus_per_node,local_rank,dist_url,world_size), nprocs=ngpus_per_node,join=False)
-    context.join(5)
+    mp.spawn(model_init, args=(ngpus_per_node,local_rank,dist_url,world_size), nprocs=ngpus_per_node,join=True)
     # end.record()
 
     # torch.cuda.synchronize()
